e762375401a74f2952d9b124178aa485c84f5214
Vineetha <vineethaalluri29@gmail.com>
2020-07-13 16:29:23 +0530

    Implemented functionality of this keyword

--- src/com/ims/actor/IMS.java
+++ src/com/ims/actor/IMS.java
@@ -3,20 +3,12 @@ package com.ims.actor;
 public class IMS {
     public static void main(String[] args){
 
-        Admin admin2 = new Admin();
-
-        admin2.name = "Sahil";
-        admin2.buildingNumber = "1171";
-        admin2.streetName = "GT Road";
-        admin2.city = "Delhi";
-        admin2.pincode = 110007;
         Admin admin = new Admin("Sahil", "1171", "GT Road", "Delhi", 110007);
-
         System.out.println(admin.name);
         System.out.println(admin.buildingNumber);
         System.out.println(admin.streetName);
         System.out.println(admin.pincode);
 
-        Admin admin3 = new Admin();
+        InventoryManager inventoryManager = new InventoryManager("Sahil", "1171", "GT Road", "Delhi", 110007);
     }
 }
--- src/com/ims/actor/InventoryManager.java
+++ src/com/ims/actor/InventoryManager.java
@@ -8,6 +8,26 @@ public class InventoryManager {
     String city;
     int pincode;
 
+    InventoryManager(){
+        System.out.println("Inventory Manager Constructed !!");
+    }
+
+    InventoryManager(String name, String buildingNumber, String streetName, String city, int pincode){
+        this();
+        this.name = name;
+        this.buildingNumber = buildingNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.pincode = pincode;
+        System.out.println("Assignments Done");
+    }
+
+    void test() {
+      this.callMe();
+    }
+    void callMe() {
+
+    }
     void checkProfileDashboard(){
         System.out.println("Please selection an option : ");
         System.out.println("Press 1 for Viewing profile details");
