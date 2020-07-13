2612d0a73ae84ad13ef72e9d985f41a1e4600359
Vineetha <vineethaalluri29@gmail.com>
2020-07-13 16:10:57 +0530

    Created Inventory Manager Class

---
+++ src/com/ims/actor/InventoryManager.java
@@ -0,0 +1,15 @@
+package com.ims.actor;
+
+public class InventoryManager {
+    int id;
+    String name;
+    String buildingNumber;
+    String streetName;
+    String city;
+    int pincode;
+
+    void checkProfileDashboard(){
+        System.out.println("Please selection an option : ");
+        System.out.println("Press 1 for Viewing profile details");
+    }
+}
