655cfc7da36c92287b5233ec030512233a596a1c
Vineetha <vineethaalluri29@gmail.com>
2020-07-13 16:16:36 +0530

    Implemented constructors for Admin class

--- src/com/ims/actor/Admin.java
+++ src/com/ims/actor/Admin.java
@@ -8,6 +8,18 @@ public class Admin {
     String city;
     int pincode;
 
+    Admin(){
+        System.out.println("Admin Constructed !!");
+    }
+
+    Admin(String n, String bn, String sn, String c, int pc){
+        name = n;
+        buildingNumber = bn;
+        streetName = sn;
+        city = c;
+        pincode = pc;
+    }
+
     void checkProfileDashboard(){
         System.out.println("Please selection an option : ");
         System.out.println("Press 1 for Viewing profile details");
