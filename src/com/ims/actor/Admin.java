f9c87ee41656e8f9c0b27a56f7b3a929ef359696
Vineetha <vineethaalluri29@gmail.com>
2020-07-13 16:04:17 +0530

    Created Admin Class

---
+++ src/com/ims/actor/Admin.java
@@ -0,0 +1,19 @@
+package com.ims.actor;
+
+public class Admin {
+    int id;
+    String name;
+    String buildingNumber;
+    String streetName;
+    String city;
+    int pincode;
+
+    void checkProfileDashboard(){
+        System.out.println("Please selection an option : ");
+        System.out.println("Press 1 for Viewing profile details");
+    }
+
+    String getName(){
+        return name;
+    }
+}
